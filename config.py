f05187a25fee8ba1f4d564caf527220ddc27a3ad
Nikulenkova <113360259+Nikulenkova@users.noreply.github.com>
2026-06-06 13:54:37 +0500

    Add a configuration template

---
+++ config.py
@@ -0,0 +1,16 @@
+DB_CONFIG = {
+    'host': '127.0.0.1',
+    'port': 5432,
+    'database': '',
+    'user': '',
+    'password': ''
+}
+
+# Токен для API HeadHunter
+HH_ACCESS_TOKEN = ''
+
+USER_AGENT = 'InternshipTracker/1.0 (your-email@example.com)'
+
+# Ссылки на ETL процессы
+URL_1 = ""
+URL_2 = ""
